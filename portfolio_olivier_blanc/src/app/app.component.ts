e610b0bb9223fabc1f18eefcbe2f6928c375916c
olivierblanc42 <olivierhumanbooster@gmail.com>
2024-12-19 16:36:49 +0100

    add

--- portfolio_olivier_blanc/src/app/app.component.ts
+++ portfolio_olivier_blanc/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import {FaIconComponent} from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FaIconComponent],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'portfolio_olivier_blanc';
+  faCoffee = faCoffee;
 }
