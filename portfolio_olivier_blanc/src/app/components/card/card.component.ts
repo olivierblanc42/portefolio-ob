814695a95a8d8561cf7f4aa8417ebaafc85c6dce
olivierblanc42 <olivierhumanbooster@gmail.com>
2025-08-08 10:11:46 +0200

    add directory component and file card

---
+++ portfolio_olivier_blanc/src/app/components/card/card.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'ui-card',
+  imports: [],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss'
+})
+export class CardComponent {
+
+}
