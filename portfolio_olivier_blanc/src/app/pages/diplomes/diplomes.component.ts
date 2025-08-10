2fb9bf8278ed2b2b676e5952c460ecf192a6db42
olivierblanc42 <olivierhumanbooster@gmail.com>
2025-08-10 11:49:31 +0200

    ts and html for diplome

--- portfolio_olivier_blanc/src/app/pages/diplomes/diplomes.component.ts
+++ portfolio_olivier_blanc/src/app/pages/diplomes/diplomes.component.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { DiplomeService } from '../../service/diplome/diplome.service';
 import { Diplome } from '../../models';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import {  faHouse } from '@fortawesome/free-solid-svg-icons';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { CardComponent } from "../../components/card/card.component";
+
 @Component({
   selector: 'app-diplomes',
-  imports: [],
+  imports: [FaIconComponent, RouterModule, CommonModule, CardComponent],
   templateUrl: './diplomes.component.html',
   standalone: true,
   styleUrl: './diplomes.component.scss'
 })
 export class DiplomesComponent implements OnInit {
 
-  diplome: Diplome[] | null = null;
-  
+  diplomes: Diplome[] | null = null;
+  faHouse = faHouse;
+
   constructor(
     private diplomeService: DiplomeService,
 
@@ -20,7 +27,7 @@ export class DiplomesComponent implements OnInit {
 
   ngOnInit(): void {
     
-  this.diplome = this.diplomeService.getDiplome();
+  this.diplomes = this.diplomeService.getDiplome();
 
   }
 
