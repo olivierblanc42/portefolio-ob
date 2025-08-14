98851d50521562e5166ad518f49f53c4d3e6b88e
olivierblanc42 <olivierhumanbooster@gmail.com>
2025-08-14 17:21:46 +0200

    css and html for media querys

--- portfolio_olivier_blanc/src/app/pages/realizations/realizations.component.ts
+++ portfolio_olivier_blanc/src/app/pages/realizations/realizations.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-realisations',
-  imports: [],
+  imports: [FaIconComponent,RouterModule,],
   templateUrl: './realizations.component.html',
   standalone: true,
   styleUrl: './realizations.component.scss'
 })
-export class RealizationsComponent {
+export class RealizationsComponent implements OnInit{
+  faHouse = faHouse;
+
+  ngOnInit(): void {
+    
+  }
 
 }
