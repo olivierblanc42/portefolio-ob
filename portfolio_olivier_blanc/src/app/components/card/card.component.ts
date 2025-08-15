abfcd8da7d98fdfc01c1e1546884ca9f70a34f66
olivierblanc42 <olivierhumanbooster@gmail.com>
2025-08-15 09:19:22 +0200

    work on responsive css html

--- portfolio_olivier_blanc/src/app/components/card/card.component.ts
+++ portfolio_olivier_blanc/src/app/components/card/card.component.ts
@@ -1,11 +1,14 @@
 import { Component, Input } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'ui-card',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
+
+  @Input() size: "card-skill" | "card-diplome" | "card-hobby" | "card-realization" = "card-skill";
 
 }
