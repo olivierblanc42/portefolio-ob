ad301bf7198d46e314655bbee018d114ef107137
olivierblanc42 <olivierhumanbooster@gmail.com>
2025-09-03 11:30:05 +0200

    change color

--- portfolio_olivier_blanc/src/app/app.routes.ts
+++ portfolio_olivier_blanc/src/app/app.routes.ts
@@ -3,12 +3,13 @@ import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'contact', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'diplomes', loadComponent: () => import('./pages/diplomes/diplomes.component').then((m) => m.DiplomesComponent) },
   { path: 'skill', loadComponent: () => import('./pages/skill/skill.component').then((m) => m.SkillComponent) },
   { path: 'loisirs', loadComponent: () => import('./pages/loisirs/loisirs.component').then((m) => m.LoisirsComponent) },
   { path: 'realisations', loadComponent: () => import('./pages/realizations/realizations.component').then((m) => m.RealizationsComponent) },
-  { path: 'contact', redirectTo: '/home', pathMatch: 'full' },
+
 
 ];
 
