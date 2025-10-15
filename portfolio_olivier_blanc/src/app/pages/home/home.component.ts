517bcea68778e331a3c44008a6d88c6e0d591ba3
olivierblanc42 <olivierhumanbooster@gmail.com>
2025-10-15 11:35:40 +0200

    change css and html

--- portfolio_olivier_blanc/src/app/pages/home/home.component.ts
+++ portfolio_olivier_blanc/src/app/pages/home/home.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import {RouterLink} from "@angular/router";
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import {faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
     imports: [
-        RouterLink
+        RouterLink ,FaIconComponent
     ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  faEnvelope = faEnvelope;
 
 }
