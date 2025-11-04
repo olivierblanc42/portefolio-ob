8791553707a6881ac400408e24465facc1ad0752
olivierblanc42 <olivierhumanbooster@gmail.com>
2025-11-04 12:49:42 +0100

    change html and css

--- portfolio_olivier_blanc/src/app/data/realizations.ts
+++ portfolio_olivier_blanc/src/app/data/realizations.ts
@@ -4,7 +4,7 @@ export const realizationList: Realization[] = [
     {
         id:1,
         name:"Aura calendar",
-        description:"Ce projet est un bot Discord créé pour le serveur Discord AURA Ligue Pokémon . Il permet de créer des événements sur un calendrier Google.",
+        description:"Ce projet est un bot Discord créé pour le serveur Discord AURA Ligue Pokémon . Il permet aux différentes boutiques présentes sur le serveur de créer des événements sur un calendrier Google.",
         urlImg:null,
         urlSite:"https://github.com/olivierblanc42/aura-Calendar",
         langage:["Javascript"],
