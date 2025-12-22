c94459ec7edbfc60905d562ef7e8e6bff8099b1b
olivierblanc42 <olivierhumanbooster@gmail.com>
2025-12-22 12:25:52 +0100

    add new realization

--- portfolio_olivier_blanc/src/app/data/realizations.ts
+++ portfolio_olivier_blanc/src/app/data/realizations.ts
@@ -2,14 +2,26 @@ import { Realization } from "../models";
 
 export const realizationList: Realization[] = [
     {
-        id:1,
-        name:"Aura calendar",
-        description:"Ce projet est un bot Discord créé pour le serveur Discord AURA Ligue Pokémon . Il permet aux différentes boutiques présentes sur le serveur de créer des événements sur un calendrier Google.",
-        urlImg:null,
-        urlSite:"https://github.com/olivierblanc42/aura-Calendar",
-        langage:["Javascript"],
+        id: 1,
+        name: "Aura calendar",
+        description: "Ce projet est un bot Discord créé pour le serveur Discord AURA Ligue Pokémon . Il permet aux différentes boutiques présentes sur le serveur de créer des événements sur un calendrier Google.",
+        urlImg: null,
+        urlSite: "https://github.com/olivierblanc42/aura-Calendar",
+        langage: ["Javascript"],
         status: "En cours de développement",
-        iconProjet: "https://i.postimg.cc/1XpCsNFF/pokeball.png"
+        iconProjet: "https://i.postimg.cc/1XpCsNFF/pokeball.png",
+        dateProject: new Date('2025-01-01')
+    },
+    {
+        id: 2,
+        name: "Moekko is love",
+        description: "Ce site a été réalisé en collaboration avec Ruru_Berryz. Le site a pour but de servir de base de données pour tous les goodies sortis dans son univers, ainsi que de présenter les artistes et leurs illustrations au cours des différents projets.",
+        urlImg: null,
+        urlSite: "https://moekkoislove.moe/",
+        langage: ["Wordpress"],
+        status: "Livré",
+        iconProjet: "https://i.postimg.cc/mgp4zVWy/moekko-is-love.webp",
+        dateProject: new Date('2025-01-01')
     }
 ]
 
